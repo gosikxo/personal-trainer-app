e66854410ce6bd2ae443f91ba66e834e6d46c879
Gosia Brozek <gosikbrozek1997@gmail.com>
2023-02-22 18:37:53 +0100

    Fix styles, and fetch and Exercise component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,47 @@
+import { useEffect, useState } from "react";
 import { Parameters } from "./components/Parameters";
 import Workout from "./components/Workout";
 
+async function fetchExcersises() {
+  const options = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "fc93ca8128msh1bb27fbaebf2010p199242jsne15137eb2e7d",
+      "X-RapidAPI-Host": "exercises-by-api-ninjas.p.rapidapi.com"
+    }
+  };
+
+  let results: string[] = [];
+  let offset = 0;
+
+  while (true) {
+    try {
+      const url = `https://exercises-by-api-ninjas.p.rapidapi.com/v1/exercises?muscle=biceps&offset=${offset}`;
+      const res = await fetch(url, options);
+      if (res.status !== 200) {
+        console.log(res);
+      }
+      const jsonedRes = await res.json();
+      if (jsonedRes.length === 0) {
+        return results;
+      } else {
+        results = results.concat(jsonedRes);
+      }
+    } catch (error) {
+      console.log(error);
+      return [];
+    }
+    offset += 10;
+  }
+}
+
 function App() {
+  const [exercises, setExcersises] = useState<string[]>();
+
+  useEffect(() => {
+    fetchExcersises().then((res) => setExcersises(res));
+  }, []);
+
   return (
     <div className="App">
       <h1>
@@ -9,7 +49,7 @@ function App() {
       </h1>
       <div className="container">
         <Parameters />
-        <Workout />
+        <Workout exercises={exercises}/>
       </div>
     </div>
   );
--- src/components/Workout.tsx
+++ src/components/Workout.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { Exercise } from './Exercise'
 
-const Workout = () => {
+const Workout = (exercises: []) => {
   return (
     <div className='workout'>
-        <h2>Workout</h2>
+      <h2>Workout</h2>
+      <Exercise exercises={exercises}/>
     </div>
   )
 }
