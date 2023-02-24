78715d900db424065e241128faaddbc6b6fa1f3e
Gosia Brozek <gosikbrozek1997@gmail.com>
2023-02-24 18:48:09 +0100

    Fetch data from json server

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,24 @@ import { useState } from "react";
 import { Parameters } from "./components/Parameters";
 import Workout from "./components/Workout";
 import { Excercise } from "./types";
+import { useEffect } from "react";
 
 function App() {
   const [exercises, setExcersises] = useState<Array<Excercise>>([]);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch('http://localhost:8000/excercises')
+      const json = await response.json()
+      setExcersises(json)
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  useEffect(() => {
+    fetchData()
+  },[])
 
   return (
     <div className="App">
