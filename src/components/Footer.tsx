6ac40db94bd856dd5fd2fbeb95110af31760618c
Gosia Brozek <gosikbrozek1997@gmail.com>
2024-03-04 13:46:04 +0100

    Add footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,16 @@
 import React from "react"
+import { FaInstagram } from "react-icons/fa6"
+import { FaTwitterSquare } from "react-icons/fa"
+import { FaFacebook } from "react-icons/fa6"
 
 function Footer() {
   return (
     <div className="footer">
+      <h3>Made by Gosia Brożek ❤️</h3>
       <div className="footer__items">
-        {/* <button className="header__item">About us</button>
-        <button className="header__item">The science behind it</button>
-        <button className="header__item">Get in touch</button> */}
+        <FaInstagram />
+        <FaTwitterSquare />
+        <FaFacebook />
       </div>
     </div>
   )
