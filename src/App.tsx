906d9aef4ab26dff9036c3ff9c2578a4e56fc7a1
Gosia Brozek <gosikbrozek1997@gmail.com>
2024-03-04 16:04:06 +0100

    Fix footer

--- src/App.tsx
+++ src/App.tsx
@@ -61,38 +61,40 @@ function App() {
 
   return (
     <div className="App">
-      <Header />
-      <h1>Meet your new personal trainer 🏋🏼‍♀️</h1>
-      <div className="container">
-        <Parameters
-          toggleAll={() => {
-            if (chosenAll) {
-              setChosenMuscleTypes({})
-            } else {
-              setChosenMuscleTypes(
-                muscleTypes.reduce(
-                  (acc, muscleType) => ({ ...acc, [muscleType]: true }),
-                  {}
+      <div className="page-container">
+        <Header />
+        <h1>Meet your new personal trainer 🏋🏼‍♀️</h1>
+        <div className="container">
+          <Parameters
+            toggleAll={() => {
+              if (chosenAll) {
+                setChosenMuscleTypes({})
+              } else {
+                setChosenMuscleTypes(
+                  muscleTypes.reduce(
+                    (acc, muscleType) => ({ ...acc, [muscleType]: true }),
+                    {}
+                  )
                 )
-              )
-              setChosenExercise("")
-            }
-          }}
-          handleChange={handleChange}
-          muscleTypes={muscleTypes}
-          chosenMuscleTypes={chosenMuscleTypes}
-          isChosenAll={chosenAll}
-        />
-      </div>
-      <div className="container">
-        <Workout
-          exercises={exercises.filter(
-            (exercise) => chosenMuscleTypes[exercise.muscle]
-          )}
-          toggleInstructions={toggleInstructions}
-          chosenExercise={chosenExercise}
-          clicked={clicked}
-        />
+                setChosenExercise("")
+              }
+            }}
+            handleChange={handleChange}
+            muscleTypes={muscleTypes}
+            chosenMuscleTypes={chosenMuscleTypes}
+            isChosenAll={chosenAll}
+          />
+        </div>
+        <div className="container">
+          <Workout
+            exercises={exercises.filter(
+              (exercise) => chosenMuscleTypes[exercise.muscle]
+            )}
+            toggleInstructions={toggleInstructions}
+            chosenExercise={chosenExercise}
+            clicked={clicked}
+          />
+        </div>
       </div>
       <Footer />
     </div>
